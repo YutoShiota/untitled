05fe3fcf5324d04925e6db89d2c8fa7e97e1ee8b
Tarutaruchiken <utan.s@me.com>
2021-06-03 13:14:18 +0900

    first commit

---
+++ assets/backgrounds/Free Furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Free Furniture" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../Free Furniture.png" width="160" height="144"/>
+</tileset>
